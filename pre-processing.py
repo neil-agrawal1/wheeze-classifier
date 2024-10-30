4e8380feae2a7fe34efc7e776a2a8f3f0063164a
Neil Agrawal <neil.agrawal12@gmail.com>
2024-10-30 13:59:30 -0400

    First commit

---
+++ pre-processing.py
@@ -0,0 +1,33 @@
+import wave
+import numpy as np
+import pandas as pd
+from scipy.signal import butter, filtfilt
+
+
+#fs = 3000;
+#sample_width = 2 (depending on number of bytes for resolution, 8-bit, 16-bit, etc)
+#channels = 1
+
+with open("audio.bin", "rb") as f:
+    audio_data = f.read()
+
+
+with wave.open("output_audio.wav", "w") as wav_file:
+    wav_file.setnchannels(channels)
+    wav_file.setsampwidth(sample_width)
+    wav_file.setframerate(fs)
+    wav_file.writeframes(audio_data)
+
+audio = np.frombuffer(audio_data, dtype=np.int16)
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs  # Nyquist frequency
+    low = lowcut / nyq  # Normalize the lowcut frequency
+    high = highcut / nyq  # Normalize the highcut frequency
+    b, a = butter(order, [low, high], btype='band', analog=False)
+    return b, a
+
+def bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = filtfilt(b, a, data)
+    return y
